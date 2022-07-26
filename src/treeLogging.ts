720569d4ad7ac5ce4814c7079f9118701729ec6e
Anaid Candido <anaid.candido@gmail.com>
2022-07-26 11:45:00 +0100

    start declaring function

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -2,10 +2,21 @@ import { Person } from "./personTypes";
 
 
 export function logAllPeopleInTree(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+    const stackedPeople = []
+    stackedPeople.push(topPerson.name)
+
+    while (stackedPeople.length >= 1) { 
+        const removedName = stackedPeople.pop()
+        console.log (removedName)
+        stackedPeople.push(topPerson.children)
+    }
+
+    
+    console.log("First person is " + topPerson.name);
 }
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
     console.log("TODO!  First person is " + topPerson.name);
 }
 
+console.log(logAllPeopleInTree())
