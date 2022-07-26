69a58dfe37a7fc01f1b55484efa2fd9d95b0e48c
Anaid Candido <anaid.candido@gmail.com>
2022-07-26 11:40:17 +0100

    fill family tree

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,23 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+    const eddardKids: Person[] = [
+        { name: "Rickon", children: [] },
+        { name: "Bran", children: [] },
+        { name: "Arya", children: [] },
+        { name: "Sansa", children: [] },
+        { name: "Robb", children: [] },
+        { name: "Jon", children: [] },
+    ]
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+    const rickardKids: Person[] = [
+        { name: "Benjen", children: [] },
+        { name: "Lyanna", children: [] },
+        { name: "Eddard", children: eddardKids },
+        { name: "Brandon", children: [] },
+    ]
+
+    const topPerson: Person = { name: "Rickard", children: rickardKids }
+
+    return topPerson
 }
