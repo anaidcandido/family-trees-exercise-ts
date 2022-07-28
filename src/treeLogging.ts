486181282800cc49e60f6d5f965b759927e4337b
Anaid Candido <anaid.candido@gmail.com>
2022-07-28 19:33:17 +0100

    wrote function to log stacked people

--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,21 +1,34 @@
 import { Person } from "./personTypes";
 
 
-export function logAllPeopleInTree(topPerson: Person): void {
+export function logAllPeopleInTree(topPerson: Person, topPersonKids: Person[]): void {
     const stackedPeople = []
     stackedPeople.push(topPerson.name)
 
-    while (stackedPeople.length >= 1) { 
-        const removedName = stackedPeople.pop()
-        console.log (removedName)
-        stackedPeople.push(topPerson.children)
+    while (stackedPeople.length > 0) {
+        const firstPerson = stackedPeople.pop()
+        console.log (firstPerson)
+        stackedPeople.push(topPersonKids)
+        const removedPerson = stackedPeople.pop()
+        console.log (removedPerson)
     }
 
     
     console.log("First person is " + topPerson.name);
 }
 
-export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
-}
+//export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
+//    const stackedPeople = []
+//    stackedPeople.push(topPerson.name)
+//
+//    while (stackedPeople.length) { 
+//        const removedName = stackedPeople.shift()
+//        console.log (removedName)
+//        stackedPeople.push(topPerson.children)
+//    }
+
+    
+//    console.log("First person is " + topPerson.name);
+//}
+
 
